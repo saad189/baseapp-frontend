636afccd5782ac238d17b765433c0b9b878b1800
mahm <mahm@ciklum.com>
2023-01-15 01:33:25 +0500

    Added app's standard navigation, header, footer components

--- src/Modules/app/app-routing.module.ts
+++ src/Modules/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MODULE_ADDRESS } from 'src/Models/module-info';
+import { NotfoundComponent } from './Components/notfound/notfound.component';
 
 const routes: Routes = [
   {
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: MODULE_ADDRESS.ABOUT_US, loadChildren: () => import('../about-us/about-us.module').then(m => m.AboutUsModule) },
   { path: MODULE_ADDRESS.CONTACT, loadChildren: () => import('../contact/contact.module').then(m => m.ContactModule) },
   { path: MODULE_ADDRESS.PRODUCTS, loadChildren: () => import('../products/products.module').then(m => m.ProductsModule) },
-  { path: MODULE_ADDRESS.EXPERTISE, loadChildren: () => import('../expertise/expertise.module').then(m => m.ExpertiseModule) }];
+  { path: MODULE_ADDRESS.EXPERTISE, loadChildren: () => import('../expertise/expertise.module').then(m => m.ExpertiseModule) },
+  { path: "**", component: NotfoundComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/Modules/app/app.module.ts
+++ src/Modules/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from '../shared/shared.module';
+import { NavigationComponent } from './Components/navigation/navigation.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { NotfoundComponent } from './Components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
